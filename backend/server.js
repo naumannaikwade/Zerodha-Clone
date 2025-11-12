c33e883b20b0c04199320b7d732ea2e2c670baae
naumannaikwade <naumannaikwade25@gmail.com>
2025-11-12 13:50:57 +0530

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://localhost:3003",
+     "https://xzerodhadashboard.onrender.com",
     ],
     credentials: true,
   })
